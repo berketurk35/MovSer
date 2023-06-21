1a4c1ec29d426b44bc79c0cf77bb5511fa852c31
Berke <berketurk@hobitek.com.tr>
2023-06-21 16:58:16 +0300

    up

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function App() {
+    return(
+        <View>
+            <Text>
+                Selam
+            </Text>
+        </View>
+    )
+};
+
+export default App;
