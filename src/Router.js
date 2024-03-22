72fe9d5ff491115c5f62b995bd0ae970ab8a88f3
Berke <forcoding2022v2@gmail.com>
2024-03-22 13:55:09 +0300

    router changes

--- src/Router.js
+++ src/Router.js
@@ -9,24 +9,11 @@ import TopNavigator2 from "./navigators/TopNavigator2";
 import TopNavigator3 from "./navigators/TopNavigator3";
 import TopNavigator4 from "./navigators/TopNavigator4";
 
-import MoviesList from "./pages/Movies/MoviesList/MoviesList";
-import ReqMoviesList from "./pages/Movies/ReqMoviesList/ReqMoviesList";
-
-import SeriesList from "./pages/Series/SeriesList/SeriesList";
-import ActiveSeriesList from "./pages/Series/ActiveSeriesList/ActiveSeriesList";
-import ReqSeriesList from "./pages/Series/ReqSeriesList/ReqSeriesList";
-
-import MyMovieList from "./pages/MyLists/MyMoviesList/MyMovieList";
-import MySeriesList from "./pages/MyLists/MySeriesList/MySeriesList";
-import MyFriendsList from "./pages/MyLists/MyFriendsList/MyFriendsList";
 
 import MovieListDetails from "./pages/MyLists/ListDetails/MovieListDetails/MovieListDetails";
 import SerieListDetails from "./pages/MyLists/ListDetails/SerieListDetails/SerieListDetails";
 import FriendListDetails from "./pages/MyLists/ListDetails/FriendListDetails/FriendListDetails";
 import FriendListDetails2 from "./pages/MyLists/ListDetails/FriendListDetails2/FriendListDetails2";
-
-import ProfileInfo from "./pages/Profile/ProfileInfo/ProfileInfo";
-import Friends from "./pages/Profile/Friends/Friends";
 
 import Intro from "./pages/Intro/Intro";
 import Login from "./pages/Login/Login";
@@ -61,27 +48,15 @@ function Router() {
                     <Stack.Screen name="TabNavigator" component={TabNavigator} />
 
                     <Stack.Screen name="TopNavigator" component={TopNavigator} />
-                    <Stack.Screen name="MoviesList" component={MoviesList} />
-                    <Stack.Screen name="ReqMoviesList" component={ReqMoviesList} />
-
                     <Stack.Screen name="TopNavigator2" component={TopNavigator2} />
-                    <Stack.Screen name="SeriesList" component={SeriesList} />
-                    <Stack.Screen name="ReqSeriesList" component={ReqSeriesList} />
-                    <Stack.Screen name="ActiveSeriesList" component={ActiveSeriesList} />
-
                     <Stack.Screen name="TopNavigator3" component={TopNavigator3} />
-                    <Stack.Screen name="MyMovieList" component={MyMovieList} />
-                    <Stack.Screen name="MySeriesList" component={MySeriesList} />
-                    <Stack.Screen name="MyFriendsList" component={MyFriendsList} />
-
+                    
                     <Stack.Screen name="MovieListDetails" component={MovieListDetails} />
                     <Stack.Screen name="SerieListDetails" component={SerieListDetails} />
                     <Stack.Screen name="FriendListDetails" component={FriendListDetails} />
                     <Stack.Screen name="FriendListDetails2" component={FriendListDetails2} />
 
                     <Stack.Screen name="TopNavigator4" component={TopNavigator4} />
-                    <Stack.Screen name="ProfileInfo" component={ProfileInfo} />
-                    <Stack.Screen name="Friends" component={Friends} />
 
                 </Stack.Navigator>
             </NavigationContainer>
